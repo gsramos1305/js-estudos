5c5330e75634088af51185e37c1b27ec69662a75
gsramos1305 <139995325+gsramos1305@users.noreply.github.com>
2023-08-08 11:53:38 -0400

    .

--- ex008/ex008.js
+++ ex008/ex008.js
@@ -1,2 +1,6 @@
 var vel = 60
-console.log(`velocidade ${vel}`)
+console.log(`velocidade: ${vel} Km/h`)
+if (vel>60) {
+    console.log('Ultrapassou o limite permitido -> Multado')
+}
+console.log('Diriga com cinto de segurança')
