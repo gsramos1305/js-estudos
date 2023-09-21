fcc2c1ec82fbcc7ac3180ef3aac4ca41f81f31ea
gsramos1305 <139995325+gsramos1305@users.noreply.github.com>
2023-09-21 11:38:56 -0400

    Update script.js

--- JS Course/Driver-licence/script.js
+++ JS Course/Driver-licence/script.js
@@ -1,9 +1,12 @@
-const age = 18
+const age = 17
+let yearsLeft = 18 - age
 
 if (age >= 18) {
     console.log(`You can drive 🚗`)
 } else {
-    const yearsLeft = (18 - age)
-    console.log(`You can't drive, wait ${yearsLeft} years`)
-
+    if (yearsLeft == 1) {
+        console.log(`You can't drive, wait ${yearsLeft} year`)
+    } else {
+        console.log(`You can't drive, wait ${yearsLeft} years`)
+    }
 }
