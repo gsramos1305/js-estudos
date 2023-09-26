c981ef74eae02e8da13987978221feb9ffc04f07
gsramos1305 <139995325+gsramos1305@users.noreply.github.com>
2023-09-26 09:35:58 -0400

    .

---
+++ JS Course/Challenge02/script.js
@@ -0,0 +1,10 @@
+let scoreDolphins = (97 + 108 + 89) / 3
+let scoreKoalas = (96 + 108 + 89) / 3
+
+if (scoreDolphins > scoreKoalas) {
+    console.log('Dolphins Wins')
+} else if (scoreDolphins === scoreKoalas) {
+    console.log('Draw')
+} else if (scoreDolphins < scoreKoalas){
+    console.log('Koalas Wins')
+}
---
+++ JS Course/Logical Operators/script.js
@@ -0,0 +1,9 @@
+const temCarteira = false
+const temBoaVisao = false
+const isTired = true
+
+if (temCarteira || temBoaVisao || isTired) {
+    console.log('Ela pode dirigir')
+} else {
+    console.log('Não pode dirigir')
+}
